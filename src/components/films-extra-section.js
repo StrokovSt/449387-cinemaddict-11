8fac2088b93e540f027671ab6653d79d6071898d
Ivan <strokovhii@gmail.comgit config --global user.name Ivangit config --global user.email strokovhii@gmail.com>
2020-04-20 17:47:18 +0700

    Задание №4. Небольшое изменение в структуре компонентов

---
+++ src/components/films-extra-section.js
@@ -0,0 +1,32 @@
+import {createElement} from "../utils.js";
+
+const createExtraFilmSectionTemplate = () => {
+  return (
+    `<section class="films-list--extra">
+      <h2 class="films-list__title">Most commented</h2>
+      <div class="films-list__container"></div>
+    </section>`
+  );
+};
+
+export default class ExtraFilmSection {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createExtraFilmSectionTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
